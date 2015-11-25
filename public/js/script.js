7310aa4e40a223b53b918f985baffa4d718b6cde
Angellus Paixão <paixao.angellus@gmail.com>
2015-11-25 20:11:53 -0200

    Home concluída. Imagens adicionadas à Experiências

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,8 @@
 
   for( var i = 0; i < sections.length; i++ ){
 
-    sections[i].style.height = window.innerHeight + "px";
-    
+    sections[i].style.minHeight = window.innerHeight + "px";
+
   }
 
 }());
