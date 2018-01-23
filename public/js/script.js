0a168deca5f1ca75d2c50d57ed045cd8a2bb6410
OI403464 <angellus.paixao@oi.net.br>
2018-01-23 15:57:18 -0200

    Item 7 adicionado e CSS organizado

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,22 @@
   }
 
 }());
+
+// Código base em: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "")
+    {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        window.location.hash = hash;
+      });
+    }
+  });
+});
